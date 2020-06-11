9332e3f7b76593a6bfc3a1facc3a80aca3addc62
butterflyred <a11025112@gmail.com>
2020-06-11 13:06:45 +0800

    revise controller

--- Controller/main.cpp
+++ Controller/main.cpp
@@ -3,8 +3,8 @@
 
 int sc_main(int argc, char *argv[]){
 
-    sc_signal<sc_uint<32> > clk;
-    sc_signal<sc_uint<32> > rst;
+    sc_signal<bool> clk;
+    sc_signal<bool> rst;
     sc_signal<sc_uint<32> > signal_stage1;
 
     Controller Controller("Controller");
@@ -15,19 +15,12 @@ int sc_main(int argc, char *argv[]){
 
     sc_trace_file *tf = sc_create_vcd_trace_file("RESULT");
 
-    sc_trace(tf, Controller.output ,"output");
+    sc_trace(tf, Controller.signal_stage1 ,"signal_stage1");
 
-    for(int i = 0; i < 7; i++) {
-        input[i] = i+2;
-    }
-
-    bias = 1;
+    
     
     sc_start(100,SC_NS);
-    for (size_t i = 0; i < 7; i++)
-    {
-        std::cout << "output[" << i << "]=" << output[i].read() << std::endl;
-    }
+   
     
     sc_close_vcd_trace_file(tf); 
 
